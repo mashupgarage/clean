4c4175248ed573e10002488ff9cb7b61140e3edb
PaoloLlenado <paollenado@gmail.com>
2024-10-09 14:51:31 +0800

    Installs prettier and eslint plugins for tailwind

--- eslint.config.js
+++ eslint.config.js
@@ -3,15 +3,17 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier";
+import tailwind from "eslint-plugin-tailwindcss";
 
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   eslintConfigPrettier,
+  ...tailwind.configs["flat/recommended"],
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     rules: { "react/react-in-jsx-scope": "off" },
-    languageOptions: { globals: globals.browser }
+    languageOptions: { globals: globals.browser },
   },
 ];
