2899a2a8de74752dab49bcc82d61b17dbb2342c7
PaoloLlenado <paollenado@gmail.com>
2024-10-09 14:54:07 +0800

    Lints styles and fixes footer alignment

--- src/pages/DrinkSelectionPage.tsx
+++ src/pages/DrinkSelectionPage.tsx
@@ -24,8 +24,8 @@ type DrinkItemProps = {
 export const DrinkSelectionPage: React.FC = () => {
   const Title = ({ label }: { label: string }) => {
     return (
-      <div className="px-4 py-2 bg-black rounded-3xl m-auto font-bold text-5xl">
-        <div className="uppercase m-auto text-white">{label}</div>
+      <div className="m-auto rounded-3xl bg-black px-4 py-2 text-5xl font-bold">
+        <div className="m-auto uppercase text-white">{label}</div>
       </div>
     );
   };
@@ -38,22 +38,22 @@ export const DrinkSelectionPage: React.FC = () => {
   }) => {
     return (
       <button
-        className="rounded-full bg-stone-800 font-semibold uppercase tracking-wide text-stone-100 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 text-5xl px-16 py-6 disabled:bg-gray-400 disabled:text-gray-200 disabled:cursor-not-allowed"
+        className="rounded-full bg-stone-800 px-16 py-6 text-5xl font-semibold uppercase tracking-wide text-stone-100 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-400 disabled:text-gray-200"
         {...props}
       >
-        <div className="uppercase m-auto text-white">{label}</div>
+        <div className="m-auto uppercase text-white">{label}</div>
       </button>
     );
   };
 
   const DrinkItem = ({ labelEn, labelCn, imageUrl, link }: DrinkItemProps) => {
     return (
-      <Link to={link} className="relative text-black font-bold text-3xl">
-        <img src={imageUrl} className="rounded-3xl block w-[600px] h-[600px]" />
-        <div className="absolute top-10 left-0 right-0 bottom-0 flex justify-center">
+      <Link to={link} className="relative text-3xl font-bold text-black">
+        <img src={imageUrl} className="block size-[600px] rounded-3xl" />
+        <div className="absolute inset-x-0 bottom-0 top-10 flex justify-center">
           {labelEn}
         </div>
-        <div className="absolute top-24 left-0 right-0 bottom-0 flex justify-center">
+        <div className="absolute inset-x-0 bottom-0 top-24 flex justify-center">
           {labelCn}
         </div>
       </Link>
@@ -61,31 +61,33 @@ export const DrinkSelectionPage: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col py-10">
-      <div className="flex flex-col gap-5">
-        <Title label={"Please choose your drink"} />
-        <Title label={"請選擇飲品"} />
+    <>
+      <div className="flex flex-col py-10">
+        <div className="flex flex-col gap-5">
+          <Title label={"Please choose your drink"} />
+          <Title label={"請選擇飲品"} />
+        </div>
+
+        <div className="mx-auto my-10 flex flex-row gap-10">
+          <DrinkItem
+            labelEn={TAP_A.labelEn}
+            labelCn={TAP_A.labelCn}
+            imageUrl={TAP_A.imageUrl}
+            link="/size/a" // TODO: decide url
+          />
+          <DrinkItem
+            labelEn={TAP_B.labelEn}
+            labelCn={TAP_B.labelCn}
+            imageUrl={TAP_B.imageUrl}
+            link="/size/b"
+          />
+        </div>
       </div>
 
-      <div className="flex flex-row gap-10 mx-auto my-10">
-        <DrinkItem
-          labelEn={TAP_A.labelEn}
-          labelCn={TAP_A.labelCn}
-          imageUrl={TAP_A.imageUrl}
-          link="/size/a" // TODO: decide url
-        />
-        <DrinkItem
-          labelEn={TAP_B.labelEn}
-          labelCn={TAP_B.labelCn}
-          imageUrl={TAP_B.imageUrl}
-          link="/size/b"
-        />
-      </div>
-
-      <div className="flex flex-row justify-between mx-10">
+      <div className="mx-10 mt-auto flex flex-row justify-between">
         <Button disabled label={"Cancel    取消"} />
         <Button disabled label={"Go back    返回"} />
       </div>
-    </div>
+    </>
   );
 };
