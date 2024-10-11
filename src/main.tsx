6ed2220418e8d0a505d8e571e0aec74cf89bc409
PaoloLlenado <paollenado@gmail.com>
2024-10-11 20:11:44 +0800

    Adds dispensing page

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { ItemSelectionPage } from "./pages/ItemSelectionPage.tsx";
 import { ItemSizePage } from "./pages/ItemSizePage.tsx";
 import { PaymentPage } from "./pages/PaymentPage.tsx";
 import { DetectCupPage } from "./pages/DetectCupPage.tsx";
+import { DispensingPage } from "./pages/DispensingPage.tsx";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
       { path: ":item/size/", element: <ItemSizePage /> },
       { path: ":item/size/:size/payment", element: <PaymentPage /> },
       { path: ":item/size/:size/detect-cup", element: <DetectCupPage /> },
+      { path: ":item/size/:size/dispense", element: <DispensingPage /> },
     ],
   },
 ]);
