ad7856c862709c47528354102823ef94b908a4b0
Mikmik28 <mikeeagregado28@gmail.com>
2024-10-16 16:47:08 +0800

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import { Outlet } from "react-router-dom";
+import styles from './styles.module.scss'
+import { useState, useEffect } from 'react'
+import { Outlet } from "react-router-dom"
+import { fetchVendingMachineAppearance } from './api/dispenser'
+import classNames from 'classnames'
 
 function App() {
+  const [backgroundColor, setBackgroundColor] = useState('')
+
+  useEffect(() => {
+    fetchVendingMachineAppearanceData()
+  }, [])
+
+  const bgStyle = classNames('#FFFFFF',
+    `bg-[${backgroundColor}]` !== ''
+  )
+
+  const fetchVendingMachineAppearanceData = async () => {
+    const { background_color } = await fetchVendingMachineAppearance()
+
+    setBackgroundColor(background_color)
+  }
+
   // Base layout
   return (
     <div
-      className="max-h-s max-h-screen min-h-screen bg-cyan-100 bg-cover"
-      // style={{ backgroundImage: "url('')" }} // Set background image
+      className={classNames(styles.wrapperClass, bgStyle)} // Set background image
     >
       <div className="h-full">
         <Outlet />
--- src/api/dispenser.ts
+++ src/api/dispenser.ts
@@ -1,9 +1,15 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-// Vending Machine Appearance
+// Vending Machine
 export const fetchVendingMachineAppearance = async () => {
   const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending-machine-appearance'
+
+  return axios.get(endpoint).then(res => res.data)
+}
+
+export const fetchVendingMachineStatus = async () => {
+  const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending_machine/status'
 
   return axios.get(endpoint).then(res => res.data)
 }
