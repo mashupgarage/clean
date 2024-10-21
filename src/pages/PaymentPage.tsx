8498a8b335cb7fbaf84688ed531d344cd3e4274c
PaoloLlenado <paollenado@gmail.com>
2024-10-21 17:55:43 +0800

    Adds modals to payment page

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -1,7 +1,8 @@
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
-import { useParams } from "react-router-dom";
-import { Item } from "../components/Item";
+import { useNavigate, useParams } from "react-router-dom";
+import { ActivityIndicator, Modal, Portal } from "react-native-paper";
+import { useState } from "react";
 
 const PAYMENT_HEADER = {
   line1: "Please choose payment method",
@@ -10,23 +11,69 @@ const PAYMENT_HEADER = {
 
 export const PaymentPage: React.FC = () => {
   const { item, size } = useParams();
-  console.log(
-    `SELECTED ITEM: ${item?.toUpperCase()} SELECTED SIZE: ${size?.toUpperCase()}`,
-  );
+  console.dir({ item, size });
+  const [visible, setVisible] = useState(false);
+  const [loadingVisible, setLoadingVisible] = useState(false);
+  const navigate = useNavigate();
+
+  const showPaymentModal = () => {
+    setLoadingVisible(true);
+    setTimeout(() => {
+      setLoadingVisible(false);
+      setVisible(true);
+    }, 2000);
+
+    setTimeout(() => {
+      setVisible(false);
+      navigate(`/${item}/size/${size}/detect-cup`);
+    }, 4000);
+  };
 
   return (
     <div className="grid h-screen w-screen grid-rows-[20%,66%,14%]">
+      <Portal>
+        <Modal
+          visible={loadingVisible}
+          onDismiss={() => setLoadingVisible(false)}
+          dismissable={false}
+          contentContainerStyle={{ height: "100%" }}
+        >
+          <ActivityIndicator animating={loadingVisible} size={"large"} />
+        </Modal>
+        <Modal
+          visible={visible}
+          onDismiss={() => setVisible(false)}
+          dismissable={false}
+          contentContainerStyle={{ height: "100%" }}
+        >
+          <div className="m-auto rounded-xl bg-white px-20 py-10 text-3xl font-semibold text-blue-500">
+            Payment Successful!
+          </div>
+        </Modal>
+      </Portal>
+
       <Header {...PAYMENT_HEADER} />
 
       <div className="flex h-full flex-row items-center justify-center">
-        <Item
-          imageUrl="https://images.squarespace-cdn.com/content/v1/52ccee75e4b00bc0dba03f46/1549025413897-WU6OP5YI319QMHUP5UI8/image-asset.png"
-          link={`/${item}/size/${size}/detect-cup`}
-        />
-        <Item
-          imageUrl="https://i0.wp.com/technode.com/wp-content/uploads/2018/09/alipay-logo-cover.jpg?fit=1600%2C920&ssl=1"
-          link={`/${item}/size/${size}/detect-cup`}
-        />
+        <div
+          onClick={showPaymentModal}
+          className="relative m-10 flex h-4/5 justify-center text-3xl font-bold uppercase text-black"
+        >
+          <img
+            className="size-full rounded-3xl"
+            src="https://images.squarespace-cdn.com/content/v1/52ccee75e4b00bc0dba03f46/1549025413897-WU6OP5YI319QMHUP5UI8/image-asset.png"
+          />
+        </div>
+
+        <div
+          onClick={showPaymentModal}
+          className="relative m-10 flex h-4/5 justify-center text-3xl font-bold uppercase text-black"
+        >
+          <img
+            className="size-full rounded-3xl"
+            src="https://i0.wp.com/technode.com/wp-content/uploads/2018/09/alipay-logo-cover.jpg?fit=1600%2C920&ssl=1"
+          />
+        </div>
       </div>
 
       <Footer />
