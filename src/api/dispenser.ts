572c176197ebc4b163be4ed7740c76fabb003877
Mikmik28 <mikeeagregado28@gmail.com>
2024-10-22 17:14:15 +0800

    Adds selectVendingMachine

--- src/api/dispenser.ts
+++ src/api/dispenser.ts
@@ -1,6 +1,14 @@
 import axios from 'axios'
+import queryString from 'query-string'
 
 // Vending Machine
+export const selectVendingMachine = async (name: string) => {
+  const params = queryString.stringify({ name })
+  const endpoint = `http://127.0.0.1:8000/api/dispenser/select-vm?${params}`
+
+  return axios.get(endpoint).then(res => res.data)
+}
+
 export const fetchVendingMachineAppearance = async () => {
   const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending-machine-appearance'
 
