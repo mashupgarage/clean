47a77c4bc3ddb68a30fd6a0ccf6fe68d0cd75d13
Mikmik28 <mikeeagregado28@gmail.com>
2024-10-29 10:07:12 +0800

    Adds dispenserTest, Refactor with baseUrl

--- src/api/dispenser.ts
+++ src/api/dispenser.ts
@@ -1,22 +1,31 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
+const baseUrl = `${import.meta.env.VITE_CLOUD_SERVER_URL}`
+
+// Dispenser
+export const dispenserTest = async () => {
+  const endpoint = `${baseUrl}/api/dispenser/test/`
+
+  return axios.get(endpoint).then(res => res.data).catch(err => err.response.data.error)
+}
+
 // Vending Machine
 export const selectVendingMachine = async (name: string) => {
   const params = queryString.stringify({ name: name })
-  const endpoint = `http://127.0.0.1:8000/api/dispenser/select-vm?${params}`
+  const endpoint = `${baseUrl}/api/dispenser/select-vm?${params}`
 
   return axios.get(endpoint).then(res => res.data).catch(err => err.response.data)
 }
 
 export const fetchVendingMachineAppearance = async () => {
-  const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending-machine-appearance'
+  const endpoint = `${baseUrl}/api/dispenser/vending-machine-appearance`
 
   return axios.get(endpoint).then(res => res.data).catch(err => err.response.data.error)
 }
 
 export const fetchVendingMachineStatus = async () => {
-  const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending_machine/status/'
+  const endpoint = `${baseUrl}/api/dispenser/vending_machine/status/`
 
   return axios.get(endpoint).then(res => res.data).catch(err => err.response.data.error)
 }
@@ -26,7 +35,7 @@ export const updateVendingMachineStatus = async (
   message: string,
   dispenser_name: string
 ) => {
-  const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending_machine/status/update/'
+  const endpoint = `${baseUrl}/api/dispenser/vending_machine/status/update/`
   const body = JSON.stringify({
     status: status,
     message: message,
@@ -37,27 +46,27 @@ export const updateVendingMachineStatus = async (
 }
 
 export const fetchLockState = async () => {
-  const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending_machine/lock-state/'
+  const endpoint = `${baseUrl}/api/dispenser/vending_machine/lock-state/`
 
   return axios.get(endpoint).then(res => res.data).catch(err => err.response.data.error)
 }
 
 export const setLockState = async (lockState: boolean) => {
-  const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending_machine/lock-state/set/'
+  const endpoint = `${baseUrl}/api/dispenser/vending_machine/lock-state/set/`
   const body = JSON.stringify({ is_locked: lockState })
 
   return axios.post(endpoint, body).then(res => res.data).catch(err => err.response.data.error)
 }
 
 export const setPinCode = async (pinCode: string) => {
-  const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending_machine/set-pin/'
+  const endpoint = `${baseUrl}/api/dispenser/vending_machine/set-pin/`
   const body = JSON.stringify({ pin_code: pinCode })
 
   return axios.post(endpoint, body).then(res => res.data).catch(err => err.response.data.error)
 }
 
 export const verifyPinCode = async (pinCode: string) => {
-  const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending_machine/verify-pin/'
+  const endpoint = `${baseUrl}/api/dispenser/vending_machine/verify-pin/`
   const body = JSON.stringify({ pin_code: pinCode })
 
   return axios.post(endpoint, body).then(res => res.data).catch(err => err.response.data.error)
