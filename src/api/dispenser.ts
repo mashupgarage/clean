c1daae013fb8631beda6a5ce9a5fdea0e9e4bc14
Mikmik28 <mikeeagregado28@gmail.com>
2024-10-29 15:38:58 +0800

    Stringify body requests

--- src/api/dispenser.ts
+++ src/api/dispenser.ts
@@ -3,7 +3,7 @@ import queryString from 'query-string'
 
 // Vending Machine
 export const selectVendingMachine = async (name: string) => {
-  const params = queryString.stringify({ name })
+  const params = queryString.stringify({ name: name })
   const endpoint = `http://127.0.0.1:8000/api/dispenser/select-vm?${params}`
 
   return axios.get(endpoint).then(res => res.data).catch(err => err.response.data)
@@ -27,11 +27,11 @@ export const updateVendingMachineStatus = async (
   dispenser_name: string
 ) => {
   const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending_machine/status/update/'
-  const body = {
+  const body = JSON.stringify({
     status: status,
     message: message,
     dispenser_name: dispenser_name,
-  }
+  })
 
   return axios.post(endpoint, body).then(res => res.data).catch(err => err.response.data[0])
 }
@@ -44,21 +44,21 @@ export const fetchLockState = async () => {
 
 export const setLockState = async (lockState: boolean) => {
   const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending_machine/lock-state/set/'
-  const body = { "is_locked": lockState }
+  const body = JSON.stringify({ is_locked: lockState })
 
   return axios.post(endpoint, body).then(res => res.data).catch(err => err.response.data.error)
 }
 
 export const setPinCode = async (pinCode: string) => {
   const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending_machine/set-pin/'
-  const body = { "pin_code": pinCode }
+  const body = JSON.stringify({ pin_code: pinCode })
 
   return axios.post(endpoint, body).then(res => res.data).catch(err => err.response.data.error)
 }
 
 export const verifyPinCode = async (pinCode: string) => {
   const endpoint = 'http://127.0.0.1:8000/api/dispenser/vending_machine/verify-pin/'
-  const body = { "pin_code": pinCode }
+  const body = JSON.stringify({ pin_code: pinCode })
 
   return axios.post(endpoint, body).then(res => res.data).catch(err => err.response.data.error)
 }
