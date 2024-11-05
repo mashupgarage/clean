0f80512a1fbab3a5ea4a643796c48cb80d8dd07c
PaoloLlenado <paollenado@gmail.com>
2024-11-05 19:59:10 +0800

    Adds function to check cup presence every 1 second

--- src/pages/DetectCupPage.tsx
+++ src/pages/DetectCupPage.tsx
@@ -1,5 +1,7 @@
-import { Header } from "../components/Header";
+import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { checkCupPresence } from "../api/dispenser";
+import { Header } from "../components/Header";
 import CountdownTimer from "../components/CountdownTimer";
 
 const DETECT_CUP_HEADER = {
@@ -7,24 +9,40 @@ const DETECT_CUP_HEADER = {
 };
 
 export const DetectCupPage: React.FC = () => {
-  const { item, size } = useParams();
   const navigate = useNavigate();
+  const { item, size } = useParams();
 
-  console.log(
-    `SELECTED ITEM: ${item?.toUpperCase()} SELECTED SIZE: ${size?.toUpperCase()}`,
-  );
+  useEffect(() => {
+    // Detect for cup presence in 1 second intervals
+    const interval = setInterval(() => {
+      if (!item) return;
+
+      // cup_status === 0 -> coffee cup not detected
+      // cup_status === 2 -> coffee cup detected by infrarerd
+      checkCupPresence(item).then((data) => {
+        if (data?.cup_status === 2) {
+          clearInterval(interval);
+          navigate(`/${item}/size/${size}/dispense`);
+        }
+      });
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(interval);
+    }, 30000);
+  }, [item]);
 
   return (
     <div className="grid h-screen w-screen grid-rows-[20%,66%,14%]">
       <Header {...DETECT_CUP_HEADER} />
 
       <div className="flex h-full flex-col items-center">
-        {item === "a" ? (
+        {item === "Tap-A" ? (
           <div
             className="relative m-10 flex w-[650px]"
             onClick={() => navigate(`/${item}/size/${size}/dispense`)}
           >
-            <img src="/media/tap.png" className="size-full rounded-3xl" />
+            <img src="/media/tap.png" className="size-full" />
           </div>
         ) : (
           <div
