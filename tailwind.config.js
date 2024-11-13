fdef971d8caa630e8d2c5e975a34baa589ea6512
PaoloLlenado <paollenado@gmail.com>
2024-11-13 19:45:11 +0800

    Removes comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,6 @@ export default {
       },
       boxShadow: {
         'item': '0px 4px 12px 0px #94a3b8;',
-        // box-shadow: 
-
-        // box-shadow: 0px 4px 4px 0px #482F2B1A;
-
       }
     },
   },
