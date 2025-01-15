63d9ed0a845d193f4b8839460e7bac4b22dfa29b
PaoloLlenado <paollenado@gmail.com>
2025-01-15 18:15:05 +0800

    Uses live date/time and product details for receipt

--- src/pages/ThankYouPage.tsx
+++ src/pages/ThankYouPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { checkCupPresence } from "../api/dispenser";
+import { checkCupPresence, getMenuItems } from "../api/dispenser";
 import { Portal } from "react-native-paper";
 import { WarningModal } from "../components/WarningModal";
 
@@ -11,8 +11,44 @@ const THANK_YOU_HEADER = {
 
 export const ThankYouPage: React.FC = () => {
   const navigate = useNavigate();
-  const { item } = useParams();
+  const { item, size } = useParams();
   const [visibleWarning, setVisibleWarning] = useState<boolean>(false);
+  const [price, setPrice] = useState<string>("");
+  const [drink, setDrink] = useState<string>("");
+  const date = new Date();
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  }).format(date);
+
+  const formattedTime = new Intl.DateTimeFormat("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  }).format(date);
+
+  useEffect(() => {
+    if (!item || !size) return; // Todo: add error catching
+
+    const fetchMenuItems = async () => {
+      const dispensers = await getMenuItems();
+      const dispenser = dispensers.find((dispenser) => dispenser.name === item);
+      if (!dispenser) {
+        return;
+      }
+      const price =
+        size === "Small" ? dispenser.price_small : dispenser.price_large;
+
+      setDrink(dispenser.drink_name);
+      setPrice(price);
+
+      // NOTE: SET PRICE TO ZERO FOR MVP
+      setPrice("0.00");
+    };
+
+    fetchMenuItems();
+  }, []);
 
   useEffect(() => {
     // Start 30 second timer, then check for cup presence
@@ -49,13 +85,14 @@ export const ThankYouPage: React.FC = () => {
           subheader="To make a new order, please remove the cup from the tap."
         />
       </Portal>
+
       <img
         src="http://localhost:5173/media/coffee-placeholder.png"
         className="absolute left-0 top-0 size-full object-cover"
       />
       <div
         className="absolute inset-0 flex flex-row items-center justify-center text-center text-white"
-        onClick={() => navigate("/item")}
+        onClick={() => navigate("/")}
       >
         <div className="w-1/2 rounded-lg bg-transparent p-12 opacity-95">
           <h1 className="text-8xl font-extrabold">{THANK_YOU_HEADER.line1}</h1>
@@ -65,8 +102,8 @@ export const ThankYouPage: React.FC = () => {
         <div className="w-[480px] bg-white p-12 text-left text-sm leading-normal text-black">
           <div className="mb-4 text-2xl font-bold">Digital Receipt</div>
 
-          <div>Date: October 23, 2024</div>
-          <div>Time: 2:15 PM</div>
+          <div>Date: {formattedDate}</div>
+          <div>Time: {formattedTime}</div>
           <div>Store Name: The Corner Store</div>
           <div>Location: 123 Main St, Anytown, USA</div>
 
@@ -74,7 +111,7 @@ export const ThankYouPage: React.FC = () => {
           <div className="mb-4 mt-1 flex flex-row justify-between">
             <div className="flex flex-col gap-2">
               <div>Product</div>
-              <div className="text-xl font-bold">Drink A</div>
+              <div className="text-xl font-bold">{drink}</div>
             </div>
             <div className="flex flex-row gap-8">
               <div className="flex flex-col gap-2">
@@ -83,15 +120,17 @@ export const ThankYouPage: React.FC = () => {
               </div>
               <div className="flex flex-col gap-2">
                 <div>Price</div>
-                <div className="text-xl font-bold">HK$ 2.00</div>
+                <div className="text-xl font-bold">HK${price}</div>
               </div>
             </div>
           </div>
 
-          <div>Subtotal: HK$ 2.00</div>
-          <div>Tax (5%): HK$0.74</div>
-          <div>Total Amount: HK$ 3.12</div>
-          <div>Payment Method: Credit Card (**** **** **** 1234)</div>
+          <div>Subtotal: HK${price}</div>
+          {/* HK does not seem to have sales tax*/}
+          {/* <div>Tax (5%): HK${price} </div> */}
+          <div>Total Amount: HK${price}</div>
+          {/* Add payment method later */}
+          <div>Payment Method: None</div>
           <div>Transaction ID: 987654321</div>
           <div>Thank you for shopping with us!</div>
           <div>Visit us again at www.thecornerstore.com</div>
