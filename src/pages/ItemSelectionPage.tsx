92a613e16e9d260da7a89db140c6ff7e9cb7bd8a
PaoloLlenado <paollenado@gmail.com>
2025-01-22 17:07:21 +0800

    Implements react query in item selection page

--- src/pages/ItemSelectionPage.tsx
+++ src/pages/ItemSelectionPage.tsx
@@ -5,42 +5,23 @@ import {
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { Item } from "../components/Item";
-// import { DispenserItem, getMenuItems } from "../api/dispenser";
+import { useQuery } from "@tanstack/react-query";
+import { fetchMenuItems } from "../api/dispenser";
 
-// TAP_A and TAP_B values can be later configured
 const ITEM_SELECTION_HEADER = {
   title: "Please choose your drink",
 };
 
-const TAP_A = {
-  name: "Sesame Oat Latte (Cold)",
-  price: "HK$ 2.20",
-  description: "Sweet and addictive nougat-like oat drink",
-  imageUrl: "/media/drink-a.png",
-};
-
-const TAP_B = {
-  name: "Red Date & Ginger Tea (Hot)",
-  price: "HK$ 2.20",
-  description: "Warming and spicy tea, low sugar",
-  imageUrl: "/media/drink-b.png",
-};
-
 export const ItemSelectionPage: React.FC = () => {
-  // NOTE: Hardcode first since loading indicators are needed when pulling from BE
-  // useEffect(() => {
-  //   const fetchMenuItems = async () => {
-  //     const dispensers = await getMenuItems();
-  //     setDispensers(dispensers);
-  //   };
-
-  //   fetchMenuItems();
-  // }, []);
-
-  // const [dispensers, setDispensers] = useState<DispenserItem[]>([]);
-  // const Tap_A = dispensers.find((dispenser) => dispenser.name === "Tap-A");
-  // const Tap_B = dispensers.find((dispenser) => dispenser.name === "Tap-B");
   const [item, setItem] = useState<string>("");
+  const { data } = useQuery({
+    queryKey: ["menuItems"],
+    queryFn: fetchMenuItems,
+  });
+
+  // TODO: Add Skeleton loaders whenever the cache is reset, OR increase stale time and only change on refresh
+  const Tap_A = data?.find((dispenser) => dispenser.name === "Tap-A");
+  const Tap_B = data?.find((dispenser) => dispenser.name === "Tap-B");
 
   return (
     <div className="grid h-screen w-screen grid-rows-[12%,63%,25%]">
@@ -48,16 +29,19 @@ export const ItemSelectionPage: React.FC = () => {
 
       <div className="flex h-full flex-row items-center justify-center">
         <Item
-          {...TAP_A}
-          // price={`HK$ ${Tap_A?.price_small}`}
-          // imageUrl={Tap_A?.drink_image}
+          imageUrl={Tap_A?.drink_image}
+          price={`HK$ ${Tap_A?.price_small}`}
+          name={Tap_A?.drink_name}
+          description={Tap_A?.drink_name2} // Note: This can be renamed to description in the backend
           stateSelection={item}
           setStateSelection={setItem}
           selection="Tap-A"
         />
         <Item
-          {...TAP_B}
-          // price={`HK$ ${Tap_B?.price_small}`}
+          imageUrl={Tap_B?.drink_image}
+          price={`HK$ ${Tap_B?.price_small}`}
+          name={Tap_B?.drink_name}
+          description={Tap_B?.drink_name2}
           stateSelection={item}
           setStateSelection={setItem}
           selection="Tap-B"
