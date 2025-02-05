0624a0787cf7146e0d8095315ae2824cda0fb4ba
PaoloLlenado <paollenado@gmail.com>
2025-02-05 15:31:14 +0800

    Temporarily removes custom button text color

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -5,7 +5,7 @@ type NavButtonProps = ComponentProps<"button"> & {
   label: string;
   onClick: () => void;
   theme?: "light" | "dark";
-  appearanceData: VendingMachineAppearance
+  appearanceData: VendingMachineAppearance;
 };
 
 export const NavButton = ({
@@ -14,16 +14,13 @@ export const NavButton = ({
   theme,
   ...props
 }: NavButtonProps) => {
-  const {
-    general_button_font_style,
-    general_button_text_color,
-  } = props.appearanceData;
+  const { general_button_font_style } = props.appearanceData;
 
   return (
     <button
       // eslint-disable-next-line tailwindcss/classnames-order
-      className={`${theme === "dark" ? "bg-black text-white" : ""} shadow-item w-[650px] rounded-full py-6 text-3xl font-bold tracking-wide text-slate-600 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-400 disabled:text-gray-200`}
-      style={{ fontFamily: general_button_font_style, color: general_button_text_color }}
+      className={`${theme === "dark" ? "bg-black text-white" : ""} w-[650px] rounded-full py-6 text-3xl font-bold tracking-wide text-slate-600 shadow-item transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-400 disabled:text-gray-200`}
+      style={{ fontFamily: general_button_font_style }}
       onClick={onClick}
       {...props}
     >
